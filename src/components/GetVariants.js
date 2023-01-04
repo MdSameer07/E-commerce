860c0bca5324645c6b9a42b52b160241a15ce961
Md <mohammedsameer772002@Gmail.com>
2023-01-04 10:53:40 +0530

    Updated some css

--- src/components/GetVariants.js
+++ src/components/GetVariants.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
+import './Variants.css'
 
 export const GetVariants = ({variants,onClick}) =>{
     return(
         variants.map((variant)=>{
             return (
-                <div><Link style = {{"textDecoration":"none"}} onClick = {()=>onClick(variant.image)}>{variant.color}</Link></div>
+                <div className = 'Variants'><Link style = {{"textDecoration":"none"}} onClick = {()=>onClick(variant.image)}>{variant.color}</Link></div>
             )
         })
     )
